2527bfa7919355442530c23156d4fce0d3fd2f55
程序小白猿 <15301072780@163.com>
2017-03-16 10:48:11 +0800

    创建单页面抓取方法

--- spider.py
+++ spider.py
@@ -1,13 +1,20 @@
 # -*- coding:utf-8 -*-
 import requests
+from requests.exceptions import RequestException
 
 
+'''
+1.获取单页的数据
+'''
 def get_one_page(url):
     try:
         response = requests.get(url)
         if response.status_code == 200:
             return  response.text
+        print("errorcode:%d" % response.status_code)
+        return None
     except RequestException:
-        print
+        print ("单页面获取失败")
+        return None
 
-    pass
+
